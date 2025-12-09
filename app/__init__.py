f327cd86edaa7379d1ca9674aa7f51a211366d0c
mattia.bianchi <mattia.bianchi@samtrevano.ch>
2025-12-09 09:08:38 +0100

    Reindirizzamento al Login

---
+++ app/__init__.py
@@ -0,0 +1,14 @@
+# __init__.py
+
+from flask import Flask
+from .routes.main import main_bp
+from .routes.auth import auth_bp
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_pyfile('config.py')
+
+    app.register_blueprint(main_bp)
+    app.register_blueprint(auth_bp, url_prefix="/auth")
+
+    return app
---
+++ app/routes/main.py
@@ -0,0 +1,9 @@
+# main.py
+
+from flask import Blueprint, redirect, url_for
+
+main_bp = Blueprint('main', __name__)
+
+@main_bp.route('/')
+def home():
+    return redirect(url_for('auth.login'))
